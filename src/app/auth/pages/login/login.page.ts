818c53e8f3595bf7c4612e87011f0770faac519f
Daniel Saluceste <daniel.saluceste2@gmail.com>
2019-09-09 16:03:32 -0300

    commit

---
+++ src/app/auth/pages/login/login.page.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+import { AuthService } from 'src/app/core/services/auth.service';
+import { AuthProvider } from 'src/app/core/services/auth.types';
+import { OverlayService } from 'src/app/core/services/overlay.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
+})
+export class LoginPage implements OnInit {
+  authForm: FormGroup;
+  authProviders = AuthProvider;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Criar uma conta'
+
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+
+  constructor(private authService: AuthService, private fb: FormBuilder, private overlayService: OverlayService) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email(): FormControl {
+    return this.authForm.get('email') as FormControl;
+  }
+
+  get name(): FormControl {
+    return this.authForm.get('name') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.authForm.get('password') as FormControl;
+  }
+
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action =  isSignIn ? 'Login' : 'Criar conta';
+    this.configs.actionChange = isSignIn ? 'Criar uma conta' : 'Eu ja tenho uma conta';
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
+  }
+
+  async onSubmit(provider: AuthProvider): Promise<void> {
+    const loading = await this.overlayService.loading();
+    try {
+      const credentials = await this.authService.authenticate({
+        isSignIn: this.configs.isSignIn,
+        user: this.authForm.value,
+        provider
+      });
+      console.log('Autenticado: ', credentials);
+      console.log('Redirecionando...');
+    } catch (e) {
+      console.log('Auth Error: ', e);
+      await this.overlayService.toast({
+        message: 'Ocorreu um erro, verifique se você digitou tudo corretamente.'
+      });
+    } finally {
+      loading.dismiss();
+    }
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBYmvuq2SXW7eC91GeFXwjc1N9rocpaTpU",
+    authDomain: "empybr.firebaseapp.com",
+    databaseURL: "https://empybr.firebaseio.com",
+    projectId: "empybr",
+    storageBucket: "",
+    messagingSenderId: "1033318416646",
+    appId: "1:1033318416646:web:5c70bd2f0f7a73cfcb9e87"
+  }
 };
 
 /*
